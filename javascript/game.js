9a437044ea9f5aede32672b128be49741afd2a1c
aditya <adipoduval5@gmail.com>
2024-03-16 16:04:05 +0530

    starting anew

--- javascript/game.js
+++ javascript/game.js
@@ -69,6 +69,12 @@ const locations = [
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart,restart, restart],
+    text: 'You die. &#x2620;'
   }
 ];
 
@@ -78,6 +84,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
