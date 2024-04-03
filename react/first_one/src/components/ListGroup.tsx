1f335815d3e8d6f1903ed0260f3f013f45c9fdfe
aditya <adipoduval5@gmail.com>
2024-04-03 10:48:32 +0530

    conditional rendering using logical and

--- react/first_one/src/components/ListGroup.tsx
+++ react/first_one/src/components/ListGroup.tsx
@@ -1,12 +1,11 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
-  if (items.length === 0) {
-    return <h3>No Items Found</h3>;
-  }
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1>List</h1>
+      {items.length === 0 && <p>There are no items</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
