573949be06770f5fa1a5dd6b8e3bf22ccf59a416
aditya <adipoduval5@gmail.com>
2024-04-06 20:46:51 +0530

    use state for highlighting selected text

--- react/first_one/src/components/ListGroup.tsx
+++ react/first_one/src/components/ListGroup.tsx
@@ -1,16 +1,27 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>There are no items</p>}
-      <ul className="list-group">
+      <ul className="list-group ">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
