612760453f3c7f2fc17cb63b680cf50b12842364
aditya <adipoduval5@gmail.com>
2024-04-08 23:06:26 +0530

    children prop

--- react/first_one/src/App.tsx
+++ react/first_one/src/App.tsx
@@ -1,9 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <Alert>
+        <h1>Hello Mom!</h1>
+      </Alert>
     </div>
   );
 }
