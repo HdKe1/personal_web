efed769fbc32e593bb190a07ce0c34ca01265ae3
aditya <adipoduval5@gmail.com>
2024-04-12 14:14:13 +0530

    on click button

--- react2/first-svelte/src/routes/+page.svelte
+++ react2/first-svelte/src/routes/+page.svelte
@@ -1,59 +1,18 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  let count = 0;
+
+  function increment() {
+    // event handler code goes here
+    count += 1;
+  }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<button on:click={increment}>
+  Clicked{count}
+  {count === 1 ? "time" : "times"}
+</button>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<button on:keydown={increment}>
+  Pressed{count}
+  {count === 1 ? "time" : "times"}
+</button>
