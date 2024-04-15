594b7279928b4feb208eed4468d69f651906ed88
aditya <adipoduval5@gmail.com>
2024-04-15 21:08:09 +0530

    added an increment button

--- react2/first-svelte/src/routes/+page.svelte
+++ react2/first-svelte/src/routes/+page.svelte
@@ -1,24 +1,8 @@
 <script>
-  import Nested from "./firstnest.svelte";
-
-  let count = 0;
-
   function increment() {
-    count += 1;
+    console.log("you pressed a button");
   }
 </script>
 
-<button on:click={increment}>
-  Clicked{count}
-  {count === 1 ? "time" : "times"}
-</button>
-
-<button on:keydown={increment}>
-  Pressed{count}
-  {count === 1 ? "time" : "times"}
-</button>
-<Nested answer={42} />
-
-{#if count > 10}
-  <p>{count} is greater than 10</p>
-{/if}
+<h1>Hello mom!</h1>
+<button on:click={increment}> Press!</button>
