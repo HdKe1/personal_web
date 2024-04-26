81a426b5368b4d172ac8b00675524eecf4a5587d
aditya <adipoduval5@gmail.com>
2024-04-26 10:09:41 +0530

    on press

--- magic_svelte/src/App.svelte
+++ magic_svelte/src/App.svelte
@@ -1,6 +1,15 @@
 <script>
+  let count = 0;
+
+  function press(event) {
+    if (event.key === "Enter") {
+      count += 1;
+    }
+  }
 </script>
 
+<button on:keydown={press}> Press Enter to Increment Count </button>
+<p>Count: {count}</p>
+
 <style>
-<style>
-    
+</style>
