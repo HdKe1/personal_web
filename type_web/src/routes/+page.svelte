6530c087a58045b36a3ab4aefa22262756b8fad5
aditya <adipoduval5@gmail.com>
2024-04-27 23:36:47 +0530

    sveltekit calculator

---
+++ type_web/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<title>Typing practice website</title>
+
+<h1>Increaseing your typing prowess</h1>
+
+<footer class="clickity">Clickity Clackity Clickity</footer>
+
+<style>
+  .clickity {
+    font-family: "Times New Roman", Times, serif;
+    font-size: small;
+  }
+</style>
