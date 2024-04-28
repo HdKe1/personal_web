1c550bfef3e661aa8f0cb4e1dafcc56925ee09b9
aditya <adipoduval5@gmail.com>
2024-04-28 22:23:56 +0530

    list

--- calculator/src/routes/+page.svelte
+++ calculator/src/routes/+page.svelte
@@ -5,11 +5,22 @@
   />
 </svelte:head>
 
-<div style="margin: 0 auto; padding: 20px; width: 700px;">
+<div style="margin: 0 auto; padding: 20px; width: 800px;">
   <h2>To do list</h2>
   <p>Enter your to do list here</p>
   <div style="display: flex;">
     <input type="text" />
-    <button style="width: 200px;">Add</button>
+    <button style="width: 200px; ">Add</button>
+  </div>
+</div>
+
+<div
+  style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;"
+>
+  <input type="checkbox" />
+  <h4 style="flex-grow: 1">I dont know</h4>
+  <div style="display: flex">
+    <button style=" width: 100px;">Edit</button>
+    <button style=" width: 100px;">Delete</button>
   </div>
 </div>
