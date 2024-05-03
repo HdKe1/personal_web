b55d9a1d29aec2fb17f954c094d0e3761941a8db
aditya <adipoduval5@gmail.com>
2024-05-03 11:44:20 +0530

    added addSnippet function

--- code-snip/src/SnippetStore.ts
+++ code-snip/src/SnippetStore.ts
@@ -1,1 +1,10 @@
  import { writable,get } from "svelte/store";
+
+ export const snippetStore = writable<CodeSnippet[]>([]);
+
+ export function addSnippet(input: CodeSnippetInput){
+    let snippets = get(snippetStore);
+    snippetStore.update(() =>{
+        return [{...input , favorite: false},...snippets]
+    })
+ }
