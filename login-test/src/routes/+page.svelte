95c19779900a84c7917b58940fa7fbb6feda791d
aditya <adipoduval5@gmail.com>
2024-05-08 08:52:04 +0530

    done with log-in button

--- login-test/src/routes/+page.svelte
+++ login-test/src/routes/+page.svelte
@@ -6,10 +6,21 @@
 <div class="p-24">
   {#if $page.data.session}
     <h1>You are logged in</h1>
+    {#if $page.data.session.user?.image}
+      <img
+        src={$page.data.session.user.image}
+        alt="user image"
+        class="w-12 h-12"
+      />
+    {/if}
+    <p>Singed in as {$page.data.session.user?.name}</p>
   {:else}
     <h1>You are not logged in</h1>
-    <button class="bg-blue-500 py-1 px-2 rounded text-white font-bold"
-      >Sign in with github</button
+    <button
+      on:click={() => signIn("github")}
+      class="bg-blue-500 py-1 px-2 rounded text-white font-bold"
+    >
+      Sign in with github</button
     >
   {/if}
 </div>
